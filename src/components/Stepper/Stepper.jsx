48834b32e814daa2aa71a644b2dea78ff35bf1cc
sergey-popov-1964 <sergeykms@gmail.com>
2022-05-13 19:44:01 +0700

    hotfix timeline

--- src/components/Stepper/Stepper.jsx
+++ src/components/Stepper/Stepper.jsx
@@ -1,43 +1,45 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./Stepper.scss";
-// import classNames from "classnames";
 import {articlePipelineStages} from '../../utils/constants'
 import StepperItem from "./StepperItem/StepperItem";
 import articleApiFetch from "../../api/ApiFetch/ArticleApiFetch";
-// import ArticleChanges from "../ArticleChanges";
 
-const Stepper = ({ className, currentStage, children, article }) => {
-  const [clickStage, setClickStage] = useState('');
+const Stepper = ({currentStage, article, openStage, onClick}) => {
   const [statuses, setStatuses] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handlerClick = (data) => {
-    setClickStage(data)
-      setIsLoading(true)
-      articleApiFetch.articleChanges(article.articleId, localStorage.getItem('user_token'),data, 0, 1000)
-        .then(res => {
-          setStatuses([])
-          setStatuses((prev) => [...prev, ...res]);
-          setIsLoading(false)
-        })
-        .catch(err => {
-          console.log(err)
-          setIsLoading(false)
-        })
-  }
+  useEffect(() => {
+    setIsLoading(true)
+    articleApiFetch.articleChanges(article.articleId, localStorage.getItem('user_token'),openStage, 0, 1000)
+      .then(res => {
+        setStatuses([])
+        setStatuses((prev) => [...prev, ...res]);
+        setIsLoading(false)
+
+      })
+      .catch(err => {
+        console.log(err)
+        setIsLoading(false)
+      })
+  }, [openStage]);
 
   return (
     articlePipelineStages.map((item, index) => (
-      <li className={`stepper-item`}>
+      <li className={ item.stage <= currentStage ? `stepper-item` : `stepper-item stepper-disabled`}>
         <div className="stepper-item__header"
-             onClick={() => handlerClick(item.stage)}
+             onClick={() => onClick(item.stage)}
         >
-          <span className={ item.stage === currentStage ? "stepper-item__icon stepper-item__icon-current text" : "stepper-item__icon text"}>{item.stage}</span>
+          <span className={ item.stage >= currentStage ? "stepper-item__icon stepper-item__icon-current text" : "stepper-item__icon text"}>{item.stage}</span>
           <h3 className="stepper-item__title text text_size_accent text_weight_bold">
             {item.title}
           </h3>
         </div>
-        <StepperItem article={article} item={item} clickStage={clickStage} statuses={statuses} isLoading={isLoading} />
+        <StepperItem article={article}
+                     item={item}
+                     statuses={statuses}
+                     isLoading={isLoading}
+                     openStage={openStage}
+        />
       </li>
     ))
   );
--- src/components/Stepper/StepperItem/StepperItem.jsx
+++ src/components/Stepper/StepperItem/StepperItem.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import "./StepperItem.scss";
 import ArticleChanges from "../../ArticleChanges";
 
-const StepperItem = ({article, item, clickStage, statuses, isLoading}) => {
+const StepperItem = ({article, item, statuses, isLoading, openStage}) => {
+
+  const myRef = useRef(null)
+
+  useEffect(() => {
+    myRef.current.scrollIntoView()
+  },[openStage, statuses]);
 
   return (
     <>
-      <div className={ clickStage && clickStage === item.stage ? "stepper-item__content article-stage" : "stepper-item__content article-stage_active"}>
-        <i className={ clickStage && clickStage === item.stage ? "stepper-item__border article-stage" : "stepper-item__border article-stage_active"}></i>
+      <div className={ openStage && openStage === item.stage ? "stepper-item__content article-stage" : "stepper-item__content article-stage_active"}>
+        <i className={ openStage && openStage === item.stage ? "stepper-item__border article-stage" : "stepper-item__border article-stage_active"}></i>
       </div>
-      <div className={clickStage && clickStage === item.stage ? "article-stage_active" : "article-stage"}>
+      <div className={openStage && openStage === item.stage ? "article-stage_active" : "article-stage"} ref={myRef}>
         <ArticleChanges article={article} stage={item.stage} statuses={statuses} isLoading={isLoading} />
       </div>
     </>
